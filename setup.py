8e30ff274de3fcdd6d109fb353278d12796ae845
Austin Bingham <austin@sixty-north.com>
2017-03-25 16:42:42 +0100

    Some initial tests.

--- setup.py
+++ setup.py
@@ -32,4 +32,12 @@ setup(name='docopt_subcommands',
       zip_safe=False,
       install_requires=requires,
       # tests_require=tests_require,
+      # List additional groups of dependencies here (e.g. development dependencies).
+      # You can install these using the following syntax, for example:
+      # $ pip install -e .[dev,test]
+      extras_require={
+          # 'dev': ['check-manifest', 'wheel'],
+          # 'doc': ['sphinx', 'cartouche'],
+          'test': ['pytest'],
+      },
       )
---
+++ test/test_main.py
@@ -0,0 +1,37 @@
+import docopt
+import docopt_subcommands as dsc
+import pytest
+
+
+@pytest.fixture()
+def reset_commands():
+    dsc._commands = {}
+
+
+@pytest.mark.usefixtures("reset_commands")
+class TestMain:
+    def test_default_top_level_help(self):
+        try:
+            dsc.main(program='prog',
+                     version='prog-1',
+                     argv=[],
+                     exit_at_end=False)
+        except docopt.DocoptExit as e:
+            assert e.usage == "Usage: prog [options] <command> [<args> ...]"
+
+    def test_default_subcommand_help(self, capsys):
+        @dsc.command('foo')
+        def foo_handler(args):
+            "usage: {program} {command}"
+            pass
+
+        try:
+            dsc.main(program='prog',
+                     version='prog-1',
+                     argv=['foo', '-h'],
+                     exit_at_end=False)
+        except SystemExit:
+            pass
+
+        out, err = capsys.readouterr()
+        assert out == "usage: prog foo\n"
