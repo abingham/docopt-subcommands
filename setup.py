466b72a9dbd583c6070f3d43b81a31e35a8dc064
Austin Bingham <austin@sixty-north.com>
2017-11-09 07:42:13 +0100

    Fixed setup.py formatting.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name='docopt_subcommands',
       classifiers=[
           'Development Status :: 4 - Beta',
           'Programming Language :: Python',
-          'Programming Language :: Python ::2.7',
+          'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
